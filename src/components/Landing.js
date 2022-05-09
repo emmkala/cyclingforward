7b9797d90a12c88d29d1550f48f908fde8e415a9
Emma Landry <emmkatlan123@gmail.com>
2022-05-09 18:22:35 -0300

    finished section 1, 2 and colour changing nav

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,11 +2,13 @@ import TopNav from './TopNav'
 
 import landingBackground from '../images/TopBackground.png'
 import animation from '../images/AssemblyFinal.gif'
+import gallery from '../images/Gallery.png'
+import topcircle from '../images/TopLeftCircle.png'
 
 import { Link } from 'react-router-dom'
 
 function Landing() {
-    var landingSections = [Section1()]
+    var landingSections = [Section1(), Section2(), Section3()]
     return (
         <div className="landing">
             <TopNav />
@@ -17,15 +19,57 @@ function Landing() {
 
 const Section1 = () => {
     return (
-        <div className="landing-1">
+        <div className="landing-sect landing-back landing-1">
             <img className="landing-background" src={landingBackground} alt="heading_image" />
             <div className="landing-info">
                 <img className="logo-animation" src={animation} alt="animation" />
                 <div className="btns">
-                    <Link className="btn-filled" to="/donation">Make a Donation.</Link>
-                    <Link className="btn-trans" to="/request">Make a Request.</Link>
+                    <Link className="btn btn-filled" to="/donation">Make a Donation.</Link>
+                    <Link className="btn btn-outline" to="/request">Make a Request.</Link>
                 </div>
             </div>
+        </div>
+    )
+}
+
+const Section2 = () => {
+    return (
+        <div className="landing-sect landing-2">
+            <div className="content row">
+                <div className="gallery-container col-6">
+                    <img className="gallery-imgs" src={gallery} alt="gallery" />
+                </div>
+                <div className="text-container col-6">
+                    <div className="background-img-container">
+                        <img className="circle-img" src={topcircle} alt="background-circle" />
+                    </div>
+                    <div className="info">
+                        <p className="italic-heading">Our Mission.</p>
+                        <p className="sub-heading">Experienced Bikes. New Purpose. </p>
+                        <p className="info-text">
+                            At Cycling Forward, we believe in second
+                            chances for everyone and everything. Working
+                            with the community, local organizations and
+                            individuals we are able to fix up and re-home
+                            previously loved bikes with people who need
+                            them free of charge. This way, we can keep
+                            bikes out of the landfill and give them a second
+                            life with those who may not have reliable
+                            transportation, who need a fun way to exercise
+                            and gain community, or who need a way to
+                            spend time with friends and family.
+                        </p>
+                        <Link className="btn btn-filled" to="/learnmore">Learn More.</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const Section3 = () => {
+    return (
+        <div className="landing-sect landing-3">
         </div>
     )
 }
--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,9 +1,29 @@
 import { NavLink } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+
 import navLogo from '../images/FinalLogo_White.png'
 
 function TopNav() {
+    const [navStyle, setNavStyle] = useState("top-nav")
+
+    const listenScrollEvent = (event) => {
+        if (window.scrollY < 73) {
+            return setNavStyle("top-nav")
+        } else if (window.scrollY > 70) {
+            return setNavStyle("full-nav")
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', listenScrollEvent);
+
+        return () =>
+            window.removeEventListener('scroll', listenScrollEvent);
+    }, []);
+
+
     return (
-        <div className="nav-bar">
+        <div className={"nav-bar " + navStyle}>
             <div className="left-nav">
                 <NavLink to="/" className="nav-link nav-btn-logo">
                     <img className="nav-img" src={navLogo} />Cycling Forward
@@ -13,7 +33,7 @@ function TopNav() {
                 <NavLink to="/learnmore" className="nav-link" id="nav-login">Learn More</NavLink>
                 <NavLink to="/shop" className="nav-link inactive">Shop</NavLink>
                 <NavLink to="/request" className="nav-link">Request</NavLink>
-                <NavLink to="/donate" className="nav-link nav-btn">Donate</NavLink>
+                <NavLink to="/donate" className="nav-link btn nav-btn">Donate</NavLink>
             </div>
         </div>
     )
